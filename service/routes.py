0f2294376f673c0ef9fe36ff705e05ecd132e567
Immanuel <euceenna@aol.com>
2026-08-24 16:58:08 -0400

    Add ci build (#17)
    * routes whitespace fix
    
    * routes whitespace fix2
    
    * lint fix3

--- service/routes.py
+++ service/routes.py
@@ -86,7 +86,7 @@ def list_accounts():
 def get_accounts(account_id):
     """Returns an Account by ID"""
     app.logger.info("Request to read Account with id: %s", account_id)
- 
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id '{account_id}' was not found.")
